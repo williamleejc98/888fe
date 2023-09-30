019ab8f37475f84382e0b57785fb973243387792
williamleejc98 <130588651+williamleejc98@users.noreply.github.com>
2023-10-01 02:52:49 +0800

    fixed

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -72,12 +72,7 @@ export const Header: React.FC<RefineThemedLayoutV2HeaderProps> = ({
   return (
     <AntdLayout.Header style={headerStyles}>
       <Space>
-        <Switch
-          checkedChildren="🌛"
-          unCheckedChildren="🔆"
-          onChange={() => setMode(mode === "light" ? "dark" : "light")}
-          defaultChecked={mode === "dark"}
-        />
+      
         {(user?.name || user?.avatar) && (
           <Space style={{ marginLeft: "8px" }} size="middle">
             {user?.name && <Text strong>{user.name}</Text>}
