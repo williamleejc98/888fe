b29561c81813f9cec931cd7ee974086c1eb351d6
williamleejc98 <130588651+williamleejc98@users.noreply.github.com>
2023-10-01 01:10:15 +0800

    Changed user creation form

--- pages/users/create/index.tsx
+++ pages/users/create/index.tsx
@@ -1,10 +1,73 @@
-import { AntdCreateInferencer } from "@refinedev/inferencer/antd";
 import { GetServerSideProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { authProvider } from "src/authProvider";
+import {  useTranslate } from "@refinedev/core";
+import { Create, useForm, useSelect } from "@refinedev/antd";
+import { Form, Input, Select } from "antd";
 
 export default function UserCreate() {
-  return <AntdCreateInferencer />;
+  const translate = useTranslate();
+  const { formProps, saveButtonProps, queryResult } = useForm();
+
+  const { selectProps: agentSelectProps } = useSelect({
+      resource: "agents",
+  });
+  return (
+    <Create saveButtonProps={saveButtonProps}>
+    <Form {...formProps} layout="vertical">
+        <Form.Item
+            label={translate("Currency")}
+            name={["currency"]}
+            rules={[
+                {
+                    required: true,
+                },
+            ]}
+        >
+            <Input />
+        </Form.Item>
+
+        <Form.Item
+            label={translate("Username")}
+            name={["memberId"]}
+            rules={[
+                {
+                    required: true,
+                },
+            ]}
+        >
+            <Input />
+        </Form.Item>
+        <Form.Item
+            label={translate("Password")}
+            name={["password"]}
+            rules={[
+                {
+                    required: true,
+                },
+            ]}
+        >
+            <Input />
+        </Form.Item>
+        <Form.Item
+            label={translate("Confirm Password")}
+            name={["passwordRepeat"]}
+            rules={[
+                {
+                    required: true,
+                },
+            ]}
+        >
+            <Input />
+        </Form.Item>
+       
+       
+     
+    </Form>
+</Create>
+
+
+  );
 }
 
 export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
