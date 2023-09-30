e2ff98c64b252b2ced4ee74c33a8287ca37791e2
williamleejc98 <130588651+williamleejc98@users.noreply.github.com>
2023-09-30 15:02:47 +0800

    fix build error for login

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -6,8 +6,12 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { authProvider } from "src/authProvider";
 import { AppIcon } from "src/components/app-icon";
 import { Create, useForm } from "@refinedev/antd";
-import { Form, Input, Button } from "antd";
+import { Form, Input, Button, message } from "antd"; // Import message from antd for displaying notifications
 import styles from "./Login.module.css"; // Import your custom CSS module
+interface LoginFormValues {
+  username: string;
+  password: string;
+}
 
 const authWrapperProps = {
   style: {
@@ -20,7 +24,7 @@ const authWrapperProps = {
 export default function Login() {
   const router = useRouter();
 
-  const onFinish = async (values) => {
+  const onFinish = async (values: LoginFormValues) => {
     try {
       const { success, error, redirectTo } = await authProvider.login(values);
   
