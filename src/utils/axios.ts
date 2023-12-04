aa57ae6731eefcea7b3c6a88effc3178812cced9
williamleejc98 <130588651+williamleejc98@users.noreply.github.com>
2023-12-04 12:37:37 +0800

    Auto logout

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,13 +1,16 @@
 import { HttpError } from "@refinedev/core";
 import axios from "axios";
+import { authProvider } from "../authProvider";
 
 const axiosInstance = axios.create();
-
 axiosInstance.interceptors.response.use(
     (response) => {
         return response;
     },
-    (error) => {
+    async (error) => {
+        if (error.response?.status === 401) {
+            // Unauthorized, log out the user
+            await authProvider.logout({});        }
         const customError: HttpError = {
             ...error,
             message: error.response?.data?.message,
