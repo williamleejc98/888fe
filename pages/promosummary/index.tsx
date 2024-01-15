3d1aa991123c37787d58d11a00230e4778944037
williamleejc98 <130588651+williamleejc98@users.noreply.github.com>
2024-01-15 20:04:43 +0800

    Revert "Revert "summary reports""
    This reverts commit c80bc1765d73f2d6d972d1109384b65282ee1530.

---
+++ pages/promosummary/index.tsx
@@ -0,0 +1,39 @@
+import { GetServerSideProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { authProvider } from "src/authProvider";
+import { useState, useEffect } from "react";
+import nookies from 'nookies';
+
+export default function PromoSummary() {
+  const iframeUrl = "https://bo.go888king.com/report/operator-performance-iframe?host_id=d2b154ee85f316a9ba2b9273eb2e3470&username=play888kingmain&event_type=1";
+
+  return (
+    <iframe 
+      src={iframeUrl} 
+      style={{ width: '100%', height: '80vh' }} 
+      frameBorder="0"
+    />
+  );
+}
+
+  
+
+  
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { authenticated, redirectTo } = await authProvider.check(context);
+
+  const translateProps = await serverSideTranslations(context.locale ?? "en", [
+    "common",
+  ]);
+  
+  if (!authenticated) {
+    return {
+      redirect: {
+        destination: `${redirectTo}?to=${encodeURIComponent("/summaries")}`,
+        permanent: false,
+      },
+    };
+  }
+
+  return { props: {} };
+};
