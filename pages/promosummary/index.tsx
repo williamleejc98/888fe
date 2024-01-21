365be67664e285cc9ac0eac3c1d47ceb77f80496
williamleejc98 <130588651+williamleejc98@users.noreply.github.com>
2024-01-21 10:20:10 +0700

    Summary

--- pages/promosummary/index.tsx
+++ pages/promosummary/index.tsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
+import nookies from 'nookies';
+const jwt = require('jsonwebtoken'); // Using jsonwebtoken
 import { GetServerSideProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { authProvider } from "src/authProvider";
-import { useState, useEffect } from "react";
-import nookies from 'nookies';
 
 export default function PromoSummary() {
-  const iframeUrl = "https://bo.go888king.com/report/operator-performance-iframe?host_id=d2b154ee85f316a9ba2b9273eb2e3470&username=play888kingmain&event_type=1";
+  const [iframeUrl, setIframeUrl] = useState("");
+
+  useEffect(() => {
+    const cookies = nookies.get();
+    const token = cookies.jwt; // Replace with your actual token name
+
+    if (token) {
+      try {
+        // Decoding token without verifying it
+        const decoded = jwt.decode(token);
+        const username = decoded.username; // Assuming 'username' is the field in your token payload
+        const newIframeUrl = `https://bo.go888king.com/report/operator-performance-iframe?host_id=d2b154ee85f316a9ba2b9273eb2e3470&username=${username}&event_type=1`;
+        setIframeUrl(newIframeUrl);
+      } catch (error) {
+        console.error("Error decoding token", error);
+      }
+    }
+  }, []);
 
   return (
     <iframe 
@@ -16,24 +34,29 @@ export default function PromoSummary() {
   );
 }
 
-  
 
-  
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
   const { authenticated, redirectTo } = await authProvider.check(context);
-
+  
   const translateProps = await serverSideTranslations(context.locale ?? "en", [
     "common",
   ]);
   
   if (!authenticated) {
     return {
+      props: {
+        ...translateProps,
+      },
       redirect: {
-        destination: `${redirectTo}?to=${encodeURIComponent("/summaries")}`,
+        destination: `${redirectTo}?to=${encodeURIComponent("/promosummaries")}`,
         permanent: false,
       },
     };
   }
-
-  return { props: {} };
-};
+  
+  return {
+    props: {
+      ...translateProps,
+    },
+  };
+  };
